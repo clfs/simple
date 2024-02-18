e1e9cce6f77ac3b84730fabbd88733151693535f
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-17 18:30:01 -0800

    Add 75 move rule to movegen

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -9,8 +9,13 @@ import "github.com/clfs/simple/core"
 //
 //   - Moves does not account for threefold reptition (9.2).
 //   - Moves does not account for fivefold reptition (9.6.1).
-//   - Moves returns no moves if the seventy-five-move rule applies (9.6.2).
+//   - Moves returns no moves if the seventy-five move rule applies (9.6.2).
 //   - Moves may return one or more moves even if the fifty-move rule applies (9.3).
 func Moves(p core.Position) []core.Move {
+	// Seventy-five move rule.
+	if p.HalfMoveClock >= 75 {
+		return nil
+	}
+
 	return nil
 }
