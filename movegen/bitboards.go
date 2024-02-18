4626863481520dcc04658d0bb5ae27384cbaa249
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-17 22:47:26 -0800

    Reduce File and Rank method counts

--- movegen/bitboards.go
+++ movegen/bitboards.go
@@ -17,88 +17,88 @@ var (
 )
 
 func init() {
-
-	// Knight attacks.
-	knightDeltas := []struct {
+	type delta struct {
 		f core.File
 		r core.Rank
-	}{
-		{2, 1}, {1, 2}, {-2, 1}, {-1, 2},
-		{2, -1}, {1, -2}, {-2, -1}, {-1, -2},
 	}
 
-	bishopDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{1, 1}, {-1, 1}, {-1, -1}, {1, -1},
-	}
-
-	rookDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{0, 1}, {1, 0}, {0, -1}, {-1, 0},
-	}
-
-	kingDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{1, 0}, {1, 1}, {0, 1}, {-1, 1},
-		{-1, 0}, {-1, -1}, {0, -1}, {1, -1},
-	}
+	var (
+		knightDeltas = []delta{{2, 1}, {1, 2}, {-2, 1}, {-1, 2}, {2, -1}, {1, -2}, {-2, -1}, {-1, -2}}
+		bishopDeltas = []delta{{1, 1}, {-1, 1}, {-1, -1}, {1, -1}}
+		rookDeltas   = []delta{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+		queenDeltas  = []delta{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
+		kingDeltas   = []delta{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
+	)
 
 	for s := core.A1; s <= core.H8; s++ {
-		if s.Rank() != core.Rank1 && s.Rank() != core.Rank8 {
+		f, r := s.File(), s.Rank()
+
+		// Pawn attacks and pushes.
+		if r != core.Rank1 && r != core.Rank8 {
 			whitePawnPushes[s].Set(s.Above())
 			blackPawnPushes[s].Set(s.Below())
-			if s.File() >= core.FileB {
+			if f >= core.FileB {
 				whitePawnAttacks[s].Set(s.Above().Left())
 				blackPawnAttacks[s].Set(s.Below().Left())
 			}
-			if s.File() <= core.FileG {
+			if f <= core.FileG {
 				whitePawnAttacks[s].Set(s.Above().Right())
 				blackPawnAttacks[s].Set(s.Below().Right())
 			}
-			if s.Rank() == core.Rank2 {
+			if r == core.Rank2 {
 				whitePawnPushes[s].Set(s.Above().Above())
 			}
-			if s.Rank() == core.Rank7 {
+			if r == core.Rank7 {
 				blackPawnPushes[s].Set(s.Below().Below())
 			}
 		}
 
+		// Knight attacks.
 		for _, d := range knightDeltas {
-			f := s.File() + d.f
-			r := s.Rank() + d.r
+			f := f + d.f
+			r := r + d.r
 			if f.Valid() && r.Valid() {
 				knightAttacks[s].Set(core.NewSquare(f, r))
 			}
 		}
+
+		// Bishop attacks.
 		for _, d := range bishopDeltas {
-			f := s.File() + d.f
-			r := s.Rank() + d.r
+			f := f + d.f
+			r := r + d.r
 			for f.Valid() && r.Valid() {
 				bishopAttacks[s].Set(core.NewSquare(f, r))
-				queenAttacks[s].Set(core.NewSquare(f, r))
 				f += d.f
 				r += d.r
 			}
 		}
+
+		// Rook attacks.
 		for _, d := range rookDeltas {
-			f := s.File() + d.f
-			r := s.Rank() + d.r
+			f := f + d.f
+			r := r + d.r
 			for f.Valid() && r.Valid() {
 				rookAttacks[s].Set(core.NewSquare(f, r))
+				f += d.f
+				r += d.r
+			}
+		}
+
+		// Queen attacks.
+		for _, d := range queenDeltas {
+			f := f + d.f
+			r := r + d.r
+			for f.Valid() && r.Valid() {
 				queenAttacks[s].Set(core.NewSquare(f, r))
 				f += d.f
 				r += d.r
 			}
 		}
-		for _, delta := range kingDeltas {
-			f := s.File() + delta.f
-			r := s.Rank() + delta.r
+
+		// King attacks.
+		for _, d := range kingDeltas {
+			f := f + d.f
+			r := r + d.r
 			if f.Valid() && r.Valid() {
 				kingAttacks[s].Set(core.NewSquare(f, r))
 			}
