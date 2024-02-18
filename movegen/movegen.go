52d1f0ae76c88466b9b6d03451758b97fe91d234
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-17 17:55:41 -0800

    Add movegen package

---
+++ movegen/movegen.go
@@ -0,0 +1,16 @@
+// Package movegen generates moves.
+package movegen
+
+import "github.com/clfs/simple/core"
+
+// Moves returns available moves in the position.
+//
+// With respect to the FIDE Laws of Chess, edition January 2023:
+//
+//   - Moves does not account for threefold reptition (9.2).
+//   - Moves does not account for fivefold reptition (9.6.1).
+//   - Moves returns no moves if the seventy-five-move rule applies (9.6.2).
+//   - Moves may return one or more moves even if the fifty-move rule applies (9.3).
+func Moves(p core.Position) []core.Move {
+	return nil
+}
