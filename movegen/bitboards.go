28d5da366a3a8f266dd458e2fccba711268f49f9
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-17 22:38:06 -0800

    Reduce for loop count

--- movegen/bitboards.go
+++ movegen/bitboards.go
@@ -56,6 +56,29 @@ func init() {
 		{2, 1}, {1, 2}, {-2, 1}, {-1, 2},
 		{2, -1}, {1, -2}, {-2, -1}, {-1, -2},
 	}
+
+	bishopDeltas := []struct {
+		f core.File
+		r core.Rank
+	}{
+		{1, 1}, {-1, 1}, {-1, -1}, {1, -1},
+	}
+
+	rookDeltas := []struct {
+		f core.File
+		r core.Rank
+	}{
+		{0, 1}, {1, 0}, {0, -1}, {-1, 0},
+	}
+
+	kingDeltas := []struct {
+		f core.File
+		r core.Rank
+	}{
+		{1, 0}, {1, 1}, {0, 1}, {-1, 1},
+		{-1, 0}, {-1, -1}, {0, -1}, {1, -1},
+	}
+
 	for s := core.A1; s <= core.H8; s++ {
 		for _, d := range knightDeltas {
 			f := s.File() + d.f
@@ -64,61 +87,26 @@ func init() {
 				knightAttacks[s].Set(core.NewSquare(f, r))
 			}
 		}
-	}
-
-	// Bishop attacks.
-	bishopDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{1, 1}, {-1, 1}, {-1, -1}, {1, -1},
-	}
-	for s := core.A1; s <= core.H8; s++ {
 		for _, d := range bishopDeltas {
 			f := s.File() + d.f
 			r := s.Rank() + d.r
 			for f.Valid() && r.Valid() {
 				bishopAttacks[s].Set(core.NewSquare(f, r))
+				queenAttacks[s].Set(core.NewSquare(f, r))
 				f += d.f
 				r += d.r
 			}
 		}
-	}
-
-	// Rook attacks.
-	rookDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{0, 1}, {1, 0}, {0, -1}, {-1, 0},
-	}
-	for s := core.A1; s <= core.H8; s++ {
 		for _, d := range rookDeltas {
 			f := s.File() + d.f
 			r := s.Rank() + d.r
 			for f.Valid() && r.Valid() {
 				rookAttacks[s].Set(core.NewSquare(f, r))
+				queenAttacks[s].Set(core.NewSquare(f, r))
 				f += d.f
 				r += d.r
 			}
 		}
-	}
-
-	// Queen attacks.
-	queenAttacks = bishopAttacks
-	for s := core.A1; s <= core.H8; s++ {
-		queenAttacks[s].With(rookAttacks[s])
-	}
-
-	// King attacks.
-	kingDeltas := []struct {
-		f core.File
-		r core.Rank
-	}{
-		{1, 0}, {1, 1}, {0, 1}, {-1, 1},
-		{-1, 0}, {-1, -1}, {0, -1}, {1, -1},
-	}
-	for s := core.A1; s <= core.H8; s++ {
 		for _, delta := range kingDeltas {
 			f := s.File() + delta.f
 			r := s.Rank() + delta.r
