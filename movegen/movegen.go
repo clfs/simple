a4308b8c7f7448e82367689996c9bba9d94bc571
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-22 18:40:54 -0800

    Update Moves and IsLegal docstrings

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -5,19 +5,18 @@ package movegen
 
 import "github.com/clfs/simple/core"
 
-// Moves returns available moves in the position.
+// Moves returns legal moves in the position.
 //
-// With respect to the FIDE Laws of Chess, edition January 2023:
-//
-//   - Moves does not account for threefold reptition (9.2).
-//   - Moves does not account for fivefold reptition (9.6.1).
-//   - Moves returns no moves if the seventy-five move rule applies (9.6.2).
-//   - Moves may return one or more moves even if the fifty-move rule applies (9.3).
+// It does not account for threefold repetition, fivefold repetition, the
+// fifty-move rule, or the seventy-five move rule.
 func Moves(p core.Position) []core.Move {
-	// Seventy-five move rule.
-	if p.HalfMoveClock >= 75 {
-		return nil
-	}
-
 	return nil
+}
+
+// IsLegal returns true if the move is legal in the position.
+//
+// It does not account for threefold repetition, fivefold repetition, the
+// fifty-move rule, or the seventy-five move rule.
+func IsLegal(p core.Position, m core.Move) bool {
+	return false
 }
