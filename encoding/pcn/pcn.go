6e13116721a2d900bda634a057a38fb2d7382f3c
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-25 13:44:31 -0800

    Add PCN package (#9)
    * Add pcn package
    
    * Simplify pcn.Decode
    
    * Clean up tests

---
+++ encoding/pcn/pcn.go
@@ -0,0 +1,84 @@
+// Package pcn implements encoding and decoding of pure coordinate notation
+// (PCN).
+package pcn
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/clfs/simple/core"
+)
+
+var encodePromotion = map[core.PieceType]string{
+	core.Knight: "n",
+	core.Bishop: "b",
+	core.Rook:   "r",
+	core.Queen:  "q",
+}
+
+// Encode encodes a move as a PCN string.
+func Encode(m core.Move) string {
+	f := strings.ToLower(m.From.String())
+	t := strings.ToLower(m.To.String())
+	p := encodePromotion[m.Promotion]
+	return f + t + p
+}
+
+func decodeSquare(s string) (core.Square, bool) {
+	if len(s) != 2 {
+		return 0, false
+	}
+
+	f := core.File(s[0] - 'a')
+	r := core.Rank(s[1] - '1')
+
+	if !f.Valid() || !r.Valid() {
+		return 0, false
+	}
+	return core.NewSquare(f, r), true
+}
+
+var decodePromotion = map[byte]core.PieceType{
+	'n': core.Knight,
+	'b': core.Bishop,
+	'r': core.Rook,
+	'q': core.Queen,
+}
+
+// Decode decodes a PCN string and returns the move it represents.
+func Decode(s string) (core.Move, error) {
+	if n := len(s); n < 4 || n > 5 {
+		return core.Move{}, fmt.Errorf("invalid length: %d", n)
+	}
+
+	from, ok := decodeSquare(s[:2])
+	if !ok {
+		return core.Move{}, fmt.Errorf("invalid start square: %s", s[:2])
+	}
+
+	to, ok := decodeSquare(s[2:4])
+	if !ok {
+		return core.Move{}, fmt.Errorf("invalid end square: %s", s[2:4])
+	}
+
+	// No promotion.
+	if len(s) == 4 {
+		return core.Move{From: from, To: to}, nil
+	}
+
+	promotion, ok := decodePromotion[s[4]]
+	if !ok {
+		return core.Move{}, fmt.Errorf("invalid promotion: %c", s[4])
+	}
+
+	return core.Move{From: from, To: to, Promotion: promotion}, nil
+}
+
+// MustDecode is like Decode but panics if the PCN is invalid.
+func MustDecode(s string) core.Move {
+	m, err := Decode(s)
+	if err != nil {
+		panic(err)
+	}
+	return m
+}
---
+++ encoding/pcn/pcn_test.go
@@ -0,0 +1,57 @@
+package pcn
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/clfs/simple/core"
+)
+
+func ExampleEncode() {
+	moves := []core.Move{
+		// Double pawn push.
+		{From: core.B2, To: core.B4},
+		// Queen promotion.
+		{From: core.F2, To: core.F1, Promotion: core.Queen},
+		// White short castle.
+		{From: core.E1, To: core.G1},
+	}
+	for _, m := range moves {
+		fmt.Println(Encode(m))
+	}
+	// Output:
+	// b2b4
+	// f2f1q
+	// e1g1
+}
+
+func TestDecode(t *testing.T) {
+	cases := []struct {
+		in      string
+		want    core.Move
+		wantErr string
+	}{
+		{in: "b2b4", want: core.Move{From: core.B2, To: core.B4}},
+		{in: "f2f1q", want: core.Move{From: core.F2, To: core.F1, Promotion: core.Queen}},
+		{in: "e1g1", want: core.Move{From: core.E1, To: core.G1}},
+		{in: "b2b4x", wantErr: "invalid promotion: x"},
+		{in: "b2b", wantErr: "invalid length: 3"},
+		{in: "b2b4qq", wantErr: "invalid length: 6"},
+		{in: "k1b2", wantErr: "invalid start square: k1"},
+		{in: "b2k1", wantErr: "invalid end square: k1"},
+	}
+
+	for _, tc := range cases {
+		got, err := Decode(tc.in)
+		if err != nil {
+			if tc.wantErr == "" {
+				t.Errorf("Decode(%q) returned error %v", tc.in, err)
+			} else if err.Error() != tc.wantErr {
+				t.Errorf("Decode(%q) returned error %v, want error %v", tc.in, err, tc.wantErr)
+			}
+		}
+		if got != tc.want {
+			t.Errorf("Decode(%q) = %v, want %v", tc.in, got, tc.want)
+		}
+	}
+}
