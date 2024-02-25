8ffeefc0bfa0d537c02a028c77a5323366508def
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-25 15:02:07 -0800

    Remove movegen.MoveFunc (#11)

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -5,11 +5,6 @@ import (
 	"github.com/clfs/simple/movegen/internal/reference"
 )
 
-// MovesFunc is the signature of a function that generates moves.
-//
-// Implementations are unable to account for threefold and fivefold repetition.
-type MovesFunc func(p core.Position) []core.Move
-
 // LegalMoves returns all legal moves in a position.
 func LegalMoves(p core.Position) []core.Move {
 	return reference.LegalMoves(p)
