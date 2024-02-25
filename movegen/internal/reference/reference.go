9c7f4a48ba5e2466679c63b74c65abc07f3b097f
Calvin Figuereo-Supraner <mail@calvin.page>
2024-02-25 08:54:08 -0800

    Add a reference move generator (#7)
    * Add a reference move generator
    
    * Remove unnecessary go generate call

---
+++ movegen/internal/reference/reference.go
@@ -0,0 +1,9 @@
+// Package reference is a reference move generator implementation.
+package reference
+
+import "github.com/clfs/simple/core"
+
+// LegalMoves returns all legal moves in a position.
+func LegalMoves(p core.Position) []core.Move {
+	return nil
+}
