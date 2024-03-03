97ea065d8229221d771225d1ee1f6a457bf67783
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-03 14:02:42 -0800

    Sort moves in movegen tests

--- movegen/internal/reference/reference_test.go
+++ movegen/internal/reference/reference_test.go
@@ -1,21 +1,23 @@
 package reference
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/clfs/simple/core"
 	"github.com/clfs/simple/encoding/fen"
 	"github.com/clfs/simple/encoding/pcn"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+// encodeMoves encodes moves as a sorted slice of PCN strings.
 func encodeMoves(t *testing.T, moves []core.Move) []string {
 	t.Helper()
 	res := make([]string, len(moves))
 	for i, m := range moves {
 		res[i] = pcn.Encode(m)
 	}
+	slices.Sort(res)
 	return res
 }
 
@@ -29,17 +31,17 @@ func TestLegalMoves(t *testing.T) {
 			name: "starting position",
 			in:   fen.Starting,
 			want: []string{
-				"a2a3", "a2a4", "b2b3", "b2b4", "c2c3", "c2c4", "d2d3", "d2d4",
-				"e2e3", "e2e4", "f2f3", "f2f4", "g2g3", "g2g4", "h2h3", "h2h4",
-				"b1a3", "b1c3", "g1f3", "g1h3",
+				"a2a3", "a2a4", "b1a3", "b1c3", "b2b3", "b2b4", "c2c3", "c2c4",
+				"d2d3", "d2d4", "e2e3", "e2e4", "f2f3", "f2f4", "g1f3", "g1h3",
+				"g2g3", "g2g4", "h2h3", "h2h4",
 			},
 		},
 		{
 			name: "center bishop",
 			in:   "k7/1R4p1/2B3P1/4b3/8/8/1P6/1K6 b - - 0 1",
 			want: []string{
-				"e5b8", "e5c7", "e5d6", "e5f4", "e5g3", "e5h2", "e5b2", "e5c3",
-				"e5d4", "e5f6",
+				"e5b2", "e5b8", "e5c3", "e5c7", "e5d4", "e5d6", "e5f4", "e5f6",
+				"e5g3", "e5h2",
 			},
 		},
 	}
@@ -47,7 +49,7 @@ func TestLegalMoves(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			moves := LegalMoves(fen.MustDecode(tc.in))
 			got := encodeMoves(t, moves)
-			if diff := cmp.Diff(tc.want, got, cmpopts.SortSlices(func(a, b string) bool { return a < b })); diff != "" {
+			if diff := cmp.Diff(tc.want, got); diff != "" {
 				t.Errorf("%q: mismatch (-want +got):\n%s", tc.in, diff)
 			}
 		})
