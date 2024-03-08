a23b3b657bd1cc7abbeb043ef115e153e0f14c86
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-08 14:47:20 -0800

    Update legal moves tests in reference movegen

--- movegen/internal/reference/reference_test.go
+++ movegen/internal/reference/reference_test.go
@@ -28,31 +28,39 @@ func TestLegalMoves(t *testing.T) {
 		want []string
 	}{
 		{
-			name: "starting position",
-			in:   fen.Starting,
-			want: []string{
+			"starting position",
+			fen.Starting,
+			[]string{
 				"a2a3", "a2a4", "b1a3", "b1c3", "b2b3", "b2b4", "c2c3", "c2c4",
 				"d2d3", "d2d4", "e2e3", "e2e4", "f2f3", "f2f4", "g1f3", "g1h3",
 				"g2g3", "g2g4", "h2h3", "h2h4",
 			},
 		},
 		{
-			name: "center bishop",
-			in:   "k7/1R4p1/2B3P1/4b3/8/8/1P6/1K6 b - - 0 1",
-			want: []string{
+			"center bishop",
+			"k7/1R4p1/2B3P1/4b3/8/8/1P6/1K6 b - - 0 1",
+			[]string{
 				"e5b2", "e5b8", "e5c3", "e5c7", "e5d4", "e5d6", "e5f4", "e5f6",
 				"e5g3", "e5h2",
 			},
 		},
 		{
-			name: "castling",
-			in:   "4k3/8/8/8/4p3/p2pPp1p/P2P1P1P/R3K2R w KQ - 0 1",
-			want: []string{
+			"castling",
+			"4k3/8/8/8/4p3/p2pPp1p/P2P1P1P/R3K2R w KQ - 0 1",
+			[]string{
 				"a1b1", "a1c1", "a1d1", "e1c1", "e1d1", "e1f1", "e1g1", "h1f1",
 				"h1g1",
 			},
 		},
+		{
+			"escape check",
+			"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+			[]string{
+				"b4c5", "c4c5", "d2d4", "f1f2", "f3d4", "g1h1",
+			},
+		},
 	}
+
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			moves := LegalMoves(fen.MustDecode(tc.in))
