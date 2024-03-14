505db8d3dfc5fbf2a8ce9262abe508f6bfbf20fe
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-13 19:57:16 -0700

    Update epd examples

--- encoding/epd/example_test.go
+++ encoding/epd/example_test.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 
 	"github.com/clfs/simple/core"
+	"github.com/clfs/simple/encoding/fen"
 	"github.com/clfs/simple/encoding/pcn"
 )
 
 func ExampleDecode() {
+	// Indian Defense: 1. d4 Nf6
+	s := `rn1qkb1r/ppp1pppp/8/3p4/3Pn3/8/PPP2PPP/RNBQKBNR w KQkq - foo bar; c0 "my comment";`
 
+	p, ops, err := Decode(s)
+	if err != nil {
+		// handle
+	}
+
+	fmt.Println(fen.Encode(p))
+	fmt.Printf("%+v\n", ops)
+	// Output:
+	// rn1qkb1r/ppp1pppp/8/3p4/3Pn3/8/PPP2PPP/RNBQKBNR w KQkq - 0 1
+	// [{Opcode:foo Args:bar} {Opcode:c0 Args:"my comment"}]
 }
 
 func ExampleEncode() {
@@ -23,7 +36,6 @@ func ExampleEncode() {
 	p.Make(pcn.MustDecode("g8f6"))
 
 	fmt.Println(Encode(p, ops))
-
 	// Output:
 	// rn1qkb1r/ppp1pppp/8/3p4/3Pn3/8/PPP2PPP/RNBQKBNR w KQkq - foo bar; c0 "my comment";
 }
