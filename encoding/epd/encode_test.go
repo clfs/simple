bb2a8b13de037b8b2ebdfb1fab0e149300e8c42e
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-15 14:29:45 -0700

    Factor out encode test cases

--- encoding/epd/encode_test.go
+++ encoding/epd/encode_test.go
@@ -7,24 +7,26 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestEncode(t *testing.T) {
-	cases := []struct {
-		in   string
-		ops  []Op
-		want string
-	}{
-		{
-			fen.Starting,
-			[]Op{
-				{"foo", "bar"},
-				{"c0", `"my comment"`},
-				{"bm", "c4 Nf3"},
-			},
-			`rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - foo bar; c0 "my comment"; bm c4 Nf3;`,
-		},
-	}
+type encodeTestCase struct {
+	in   string
+	ops  []Op
+	want string
+}
 
-	for i, tc := range cases {
+var encodeTestCases = []encodeTestCase{
+	{
+		fen.Starting,
+		[]Op{
+			{"foo", "bar"},
+			{"c0", `"my comment"`},
+			{"bm", "c4 Nf3"},
+		},
+		`rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - foo bar; c0 "my comment"; bm c4 Nf3;`,
+	},
+}
+
+func TestEncode(t *testing.T) {
+	for i, tc := range encodeTestCases {
 		got := Encode(fen.MustDecode(tc.in), tc.ops)
 		if diff := cmp.Diff(tc.want, got); diff != "" {
 			t.Errorf("#%d: Encode() mismatch: (-want +got):\n%s", i, diff)
