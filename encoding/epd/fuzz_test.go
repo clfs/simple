d0505ceb993b0b909183d121bcf782ef1e1f1564
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-15 14:15:21 -0700

    Add a fuzz test

--- encoding/epd/fuzz_test.go
+++ encoding/epd/fuzz_test.go
@@ -1,1 +1,18 @@
 package epd
+
+import "testing"
+
+func FuzzRoundTrip(f *testing.F) {
+	f.Add(`rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - foo bar; c0 "my comment"; bm c4 Nf3;`)
+	f.Fuzz(func(t *testing.T, s string) {
+		p, ops, err := Decode(s)
+		if err != nil {
+			return
+		}
+
+		s2 := Encode(p, ops)
+		if s != s2 {
+			t.Errorf("changed after round trip: %q -> %q", s, s2)
+		}
+	})
+}
