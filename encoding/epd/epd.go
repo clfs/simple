ccb5a19d6135bf36615afff2c1a11a36ce226b86
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-16 12:13:25 -0700

    Update docstring

--- encoding/epd/epd.go
+++ encoding/epd/epd.go
@@ -1,9 +1,6 @@
 // Package epd implements encoding and decoding of Extended Position Description
 // (EPD) as defined in "Standard: Portable Game Notation Specification and
 // Implementation Guide", revision 1994.03.12.
-//
-// This package makes the simplifying assumption that all EPD operations contain
-// at most one operand.
 package epd
 
 // An Op is an EPD operation.
