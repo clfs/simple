d4db742451f04dfd3eac83d0be0f1d533bdc6c34
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-17 17:17:41 -0700

    Add Decode function

--- internal/encoding/epd/epd.go
+++ internal/encoding/epd/epd.go
@@ -1,9 +1,91 @@
+// Package epd implements decoding for the Extended Position Description format.
 package epd
 
-import "github.com/clfs/simple/core"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/clfs/simple/core"
+	"github.com/clfs/simple/encoding/fen"
+)
+
+// Starting is the starting position in EPD format.
+const Starting = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -"
 
 // ExtendedPosition represents an EPD position.
 type ExtendedPosition struct {
 	Position core.Position
 	Comment  string
+	Perft    []int
+}
+
+func Decode(s string) (ExtendedPosition, error) {
+	var res ExtendedPosition
+
+	fields := strings.SplitN(s, " ", 5)
+	if n := len(fields); n < 4 {
+		return ExtendedPosition{}, fmt.Errorf("too few fields: %d", n)
+	}
+
+	// Decode the position.
+	pseudoFEN := strings.Join(fields[:4], " ") + " 0 1"
+	p, err := fen.Decode(pseudoFEN)
+	if err != nil {
+		return ExtendedPosition{}, err
+	}
+
+	res.Position = p
+
+	// Return early if there are no operations.
+	if len(fields) == 4 {
+		return res, nil
+	}
+
+	// Decode the operations.
+	for ops := fields[4]; len(ops) > 0; {
+		var (
+			op string
+			ok bool
+		)
+
+		op, ops, ok = strings.Cut(ops, ";")
+		if !ok {
+			return ExtendedPosition{}, fmt.Errorf("invalid operations")
+		}
+
+		op = strings.TrimSpace(op)
+
+		opcode, args, _ := strings.Cut(op, " ")
+
+		switch opcode {
+		case "fmvn":
+			n, err := strconv.Atoi(args)
+			if err != nil {
+				return ExtendedPosition{}, fmt.Errorf("invalid fmvn")
+			}
+			res.Position.FullMoveNumber = n
+		case "hmvc":
+			n, err := strconv.Atoi(args)
+			if err != nil {
+				return ExtendedPosition{}, fmt.Errorf("invalid hmvc")
+			}
+			res.Position.HalfMoveClock = n
+		case "c0":
+			res.Comment = args
+		case "perft":
+			depths := strings.Split(args, " ")
+			for _, d := range depths {
+				n, err := strconv.Atoi(d)
+				if err != nil {
+					return ExtendedPosition{}, fmt.Errorf("invalid perft")
+				}
+				res.Perft = append(res.Perft, n)
+			}
+		default:
+			return ExtendedPosition{}, fmt.Errorf("unknown opcode: %s", opcode)
+		}
+	}
+
+	return res, nil
 }
---
+++ internal/encoding/epd/epd_test.go
@@ -0,0 +1,43 @@
+package epd
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/clfs/simple/core"
+	"github.com/clfs/simple/encoding/fen"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDecode(t *testing.T) {
+	cases := []struct {
+		in      string
+		want    ExtendedPosition
+		wantErr bool
+	}{
+		{
+			in: Starting,
+			want: ExtendedPosition{
+				Position: core.NewPosition(),
+			},
+		},
+		{
+			in: Starting + " hmvc 4; fmvn 3;",
+			want: ExtendedPosition{
+				Position: fen.MustDecode("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 4 3"),
+			},
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got, err := Decode(tc.in)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("Decode(%q) returned error: %v", tc.in, err)
+			}
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("Decode(%q) returned unexpected result (-want +got):\n%s", tc.in, diff)
+			}
+		})
+	}
+}
