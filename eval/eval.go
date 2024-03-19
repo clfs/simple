abb581b664163d08cfde8c0fe48a4563bebd3d69
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-18 18:53:00 -0700

    Have eval.Eval take a value

--- eval/eval.go
+++ eval/eval.go
@@ -6,6 +6,6 @@ import "github.com/clfs/simple/core"
 // Eval returns the relative value of the position. Positive values indicate an
 // advantage for the side to move, while negative values indicate an advantage
 // for the opponent.
-func Eval(p *core.Position) int {
+func Eval(p core.Position) int {
 	return 0
 }
