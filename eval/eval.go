2c5899521112c2f47b0c8b5a619cb1247fe5fc02
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-18 18:52:38 -0700

    Add eval.Eval

---
+++ eval/eval.go
@@ -0,0 +1,11 @@
+// Package eval implements positional evaluation.
+package eval
+
+import "github.com/clfs/simple/core"
+
+// Eval returns the relative value of the position. Positive values indicate an
+// advantage for the side to move, while negative values indicate an advantage
+// for the opponent.
+func Eval(p *core.Position) int {
+	return 0
+}
