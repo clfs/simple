aa1e79894240936181a748fa636c9274db2e7d49
Calvin Figuereo-Supraner <mail@calvin.page>
2024-03-19 22:25:40 -0700

    Add eval package

--- eval/eval.go
+++ eval/eval.go
@@ -1,63 +1,11 @@
 // Package eval implements positional evaluation.
 package eval
 
-import (
-	"github.com/clfs/simple/core"
-	"github.com/clfs/simple/movegen"
-)
+import "github.com/clfs/simple/core"
 
-var pieceTypeWeights = map[core.PieceType]int{
-	core.Pawn:   100,
-	core.Knight: 300,
-	core.Bishop: 300,
-	core.Rook:   500,
-	core.Queen:  900,
-}
-
-// Eval returns the relative value of the position. Positive values indicate an
-// advantage for the side to move, while negative values indicate an advantage
-// for the opponent.
-func Eval(p core.Position) int {
-	friendlyValue := value(p)
-	p.SideToMove = p.SideToMove.Other()
-	opponentValue := value(p)
-	return friendlyValue - opponentValue
-}
-
-func value(p core.Position) int {
-	var res int
-
-	// Reward piece presence.
-	for s := core.A1; s <= core.H8; s++ {
-		piece, ok := p.Board.Get(s)
-		if ok && piece.Color() == p.SideToMove {
-			res += pieceTypeWeights[piece.Type()]
-		}
-	}
-
-	// Penalize weak pawn structures.
-	dp := doubledPawns(p)
-	bp := blockedPawns(p)
-	ip := isolatedPawns(p)
-	res -= 50 * (dp.Count() + bp.Count() + ip.Count())
-
-	// Reward mobility.
-	res += 10 * len(movegen.LegalMoves(p))
-
-	return res
-}
-
-// doubledPawns returns a bitboard of all doubled pawns for the side to move.
-func doubledPawns(_ core.Position) core.Bitboard {
-	return 0
-}
-
-// blockedPawns returns a bitboard of all blocked pawns for the side to move.
-func blockedPawns(_ core.Position) core.Bitboard {
-	return 0
-}
-
-// isolatedPawns returns a bitboard of all isolated pawns for the side to move.
-func isolatedPawns(_ core.Position) core.Bitboard {
-	return 0
-}
+// EvalFunc is the signature of an evaluation function used to determine the
+// relative value of a position.
+//
+// Positive values indicate an advantage for the side to move, and negative
+// values indicate an advantage for the opponent.
+type EvalFunc func(core.Position) int
---
+++ eval/reference/reference_test.go
@@ -0,0 +1,34 @@
+package reference
+
+import (
+	"testing"
+
+	"github.com/clfs/simple/encoding/fen"
+)
+
+func TestEval(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{
+			fen.Starting,
+			0,
+		},
+		{
+			"4k3/8/8/8/8/1R6/8/4K3 w - - 0 1",
+			500,
+		},
+		{
+			"4k3/8/8/8/8/1R6/8/4K3 b - - 0 1",
+			-500,
+		},
+	}
+
+	for _, tc := range cases {
+		got := Eval(fen.MustDecode(tc.in))
+		if tc.want != got {
+			t.Errorf("%q: want %d, got %d", tc.in, tc.want, got)
+		}
+	}
+}
